74697d9c8f3381475f544e3f39bb5f2e5ac77e88
Patrick Sardinha <patricksardinha.pro@gmail.com>
2024-03-29 19:08:24 +0100

    spotlight component & animation tailwind

--- portfolio-app/app/page.tsx
+++ portfolio-app/app/page.tsx
@@ -1,10 +1,20 @@
-import { Button } from "@/components/ui/button";
+import { Spotlight } from "@/components/ui/Spotlight";
 
 export default function Home() {
   return (
-    <div>
-      Portfolio
-      <Button>Click me</Button>
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Patrick Sardinha. <br />
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          I am Fullstack developper, Welcome to my website.
+        </p>
+      </div>
     </div>
   );
 }
