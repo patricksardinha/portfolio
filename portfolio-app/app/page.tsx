d0121f092f31793a5c84e9dfcdb467f7c146f682
Patrick Sardinha <patricksardinha.pro@gmail.com>
2024-03-30 16:08:38 +0100

    add globe & lamp components

--- portfolio-app/app/page.tsx
+++ portfolio-app/app/page.tsx
@@ -1,9 +1,13 @@
+import Contact from "./Contact";
+import Experiences from "./Experiences";
 import Header from "./Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Experiences />
+      <Contact />
     </>
   );
 }
