f568f31ec7104bccfccf4fe7f0943e449e40d0d5
Patrick Sardinha <patricksardinha.pro@gmail.com>
2024-03-30 17:03:08 +0100

    impl divider generic component

--- portfolio-app/app/page.tsx
+++ portfolio-app/app/page.tsx
@@ -1,13 +1,19 @@
+import Divider from "@/components/ui/Divider";
 import Contact from "./Contact";
 import Experiences from "./Experiences";
+import Footer from "./Footer";
 import Header from "./Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Divider titleSection="Experience" />
       <Experiences />
+      <Divider titleSection="Contact" />
       <Contact />
+      <Divider titleSection="Footer" />
+      <Footer />
     </>
   );
 }
