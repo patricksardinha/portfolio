f336c65873b04badc525ea8695014d6be1f523d3
Patrick Sardinha <patricksardinha.pro@gmail.com>
2024-04-04 01:41:53 +0200

    cursus content & update fix warning footer

--- portfolio-app/components/Cursus.tsx
+++ portfolio-app/components/Cursus.tsx
@@ -7,8 +7,9 @@ export default function Cursus() {
         <ol className="flex gap-x-16 justify-center items-end relative">
           <li className="w-1/4 ms-4">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Application UI code in Tailwind CSS
+              University of Geneva
             </h3>
+            <h4>Bachelor of Science - Computer Sciences</h4>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
               Get access to over 20+ pages including a dashboard layout, charts,
               kanban board, calendar, and pre-order E-commerce & Marketing
@@ -16,21 +17,22 @@ export default function Cursus() {
             </p>
 
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              February 2022
+              Year 2016 - 2019
             </time>
             <div className="relative w-3 h-3 bg-gray-200 rounded-full -bottom-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           </li>
 
           <li className="w-1/4 ms-4">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              E-Commerce UI code in Tailwind CSS
+              Honk-Kong University of Science and Technology - Coursera
             </h3>
+            <h4>Full-Stack Web Development Formation</h4>
             <p className="text-base font-normal text-gray-500 dark:text-gray-400">
               Get started with dozens of web components and interactive elements
               built on top of Tailwind CSS.
             </p>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              April 2022
+              Year 2021 - 2022
             </time>
             <div className="relative w-3 h-3 bg-gray-200 rounded-full -bottom-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           </li>
@@ -40,47 +42,28 @@ export default function Cursus() {
           <li className="w-1/4 ms-4">
             <div className="relative w-3 h-3 bg-gray-200 rounded-full -top-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              February 2022
+              Year 2013 - 2016
             </time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Application UI code in Tailwind CSS
+              Lycée de la Versoie
             </h3>
+            <h4>General baccalaureate - Scientific Series</h4>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
               Get access to over 20+ pages including a dashboard layout, charts,
               kanban board, calendar, and pre-order E-commerce & Marketing
               pages.
             </p>
-            <a
-              href="#"
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-            >
-              Learn more{" "}
-              <svg
-                className="w-3 h-3 ms-2 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
           </li>
 
           <li className="w-1/4 ms-4">
             <div className="relative w-3 h-3 bg-gray-200 rounded-full -top-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              March 2022
+              Year 2019 - 2021
             </time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Marketing UI design in Figma
+              University of Geneva
             </h3>
+            <h4>Master of Science - Computer Sciences</h4>
             <p className="text-base font-normal text-gray-500 dark:text-gray-400">
               All of the pages and components are first designed in Figma and we
               keep a parity between the two versions even as we update the
@@ -91,37 +74,17 @@ export default function Cursus() {
           <li className="w-1/4 ms-4">
             <div className="relative w-3 h-3 bg-gray-200 rounded-full -top-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              February 2022
+              Year 2022 - 2023
             </time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Application UI code in Tailwind CSS
+              Unity Technologies
             </h3>
+            <h4>Unity Programmer Formation</h4>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
               Get access to over 20+ pages including a dashboard layout, charts,
               kanban board, calendar, and pre-order E-commerce & Marketing
               pages.
             </p>
-            <a
-              href="#"
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-            >
-              Learn more{" "}
-              <svg
-                className="w-3 h-3 ms-2 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
           </li>
         </ol>
       </div>
@@ -141,27 +104,6 @@ export default function Cursus() {
               kanban board, calendar, and pre-order E-commerce & Marketing
               pages.
             </p>
-            <a
-              href="#"
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-            >
-              Learn more{" "}
-              <svg
-                className="w-3 h-3 ms-2 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
           </li>
           <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
