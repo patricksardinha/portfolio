3317ba463067482289b645adc012eeb6b3bf58fc
Patrick Sardinha <patricksardinha.pro@gmail.com>
2024-04-06 02:52:52 +0200

    update cursus with content + mobile version + card template on experiences

--- portfolio-app/components/Cursus.tsx
+++ portfolio-app/components/Cursus.tsx
@@ -81,9 +81,10 @@ export default function Cursus() {
             </h3>
             <h4>Unity Programmer Formation</h4>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-              I also trained in the development of real-time 3D applications
-              with Unity. At the same time, I also learned to work with 2D and
-              3D modeling software.
+              I also did a formation in the development of real-time 3D
+              applications with Unity & Unreal Engine. At the same time, I
+              learned to work with 2D and 3D modeling software like Blender for
+              exemple.
             </p>
           </li>
         </ol>
@@ -94,42 +95,80 @@ export default function Cursus() {
           <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              February 2022
+              Year 2013 - 2016
             </time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Application UI code in Tailwind CSS
+              Lycée de la Versoie
             </h3>
+            <h4>General baccalaureate - Scientific Series</h4>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-              Get access to over 20+ pages including a dashboard layout, charts,
-              kanban board, calendar, and pre-order E-commerce & Marketing
-              pages.
+              In high school I discovered programming through the Computer
+              Science and Digital Sciences specialty and that{"'"}s where my
+              journey began !
             </p>
           </li>
+
           <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              March 2022
+              Year 2016 - 2019
             </time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Marketing UI design in Figma
+              University of Geneva
             </h3>
-            <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-              All of the pages and components are first designed in Figma and we
-              keep a parity between the two versions even as we update the
-              project.
+            <h4>Bachelor of Science - Computer Sciences</h4>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              My learning during my bachelor{"'"}s degree was very enriching. I
+              acquired a lot of knowledge about IT in the broad sense and I had
+              the opportunity to participate in a lot of practical work.
             </p>
           </li>
-          <li className="ms-4">
+
+          <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              April 2022
+              Year 2019 - 2021
             </time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              E-Commerce UI code in Tailwind CSS
+              University of Geneva
             </h3>
+            <h4>Master of Science - Computer Sciences</h4>
             <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-              Get started with dozens of web components and interactive elements
-              built on top of Tailwind CSS.
+              During my master{"'"}s degree I gained more and more knowledge and
+              I had the opportunity to put them into practice on large-scale
+              academic projects.
+            </p>
+          </li>
+
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              Year 2021 - 2022
+            </time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Honk-Kong University of Science and Technology - Coursera
+            </h3>
+            <h4>Full-Stack Web Development Formation</h4>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              After my university training I decided to follow a formation in
+              Full Stack Web development to further specialize in this field.
+            </p>
+          </li>
+
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              Year 2022 - 2023
+            </time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Unity Technologies
+            </h3>
+            <h4>Unity Programmer Formation</h4>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              I also did a formation in the development of real-time 3D
+              applications with Unity & Unreal Engine. At the same time, I
+              learned to work with 2D and 3D modeling software like Blender for
+              exemple.
             </p>
           </li>
         </ol>
